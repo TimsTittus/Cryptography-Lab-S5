d76f6a2e4c6bac1f190b4692da3446b7eb93862a
Tims Tittus <151863700+TimsTittus@users.noreply.github.com>
2025-10-15 14:25:07 +0530

    Refactor Digital Signature class and methods

--- Digital Signature/signature.java
+++ Digital Signature/signature.java
@@ -1,46 +1,45 @@
 import java.security.*;
 import java.util.Base64;
 
-public class signature {
+public class DigitalSignatureDemo {
 
     private static final String ALGORITHM = "SHA256withRSA";
     private static final int KEY_SIZE = 2048;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        try {
+            KeyPair keyPair = generateKeyPair();
+            PublicKey publicKey = keyPair.getPublic();
+            PrivateKey privateKey = keyPair.getPrivate();
 
-        KeyPair keyPair = generateKeyPair();
-        PublicKey publicKey = keyPair.getPublic();
-        PrivateKey privateKey = keyPair.getPrivate();
+            String originalMessage = "This is a secret message to be digitally signed.";
+            System.out.println("Original Message: " + originalMessage);
 
-        String originalMessage = "This is a secret message to be digitally signed.";
-        System.out.println("Original Message: " + originalMessage);
+            byte[] signatureBytes = signMessage(originalMessage.getBytes(), privateKey);
+            String signatureBase64 = Base64.getEncoder().encodeToString(signatureBytes);
+            System.out.println("Digital Signature (Base64): " + signatureBase64);
 
-        byte[] digitalSignature = signMessage(originalMessage.getBytes(), privateKey);
-
-        boolean isVerified = verifySignature(originalMessage.getBytes(), digitalSignature, publicKey);
-        System.out.println("Signature Verified: " + isVerified);
-
-        String tamperedMessage = "This is a tampered message.";
-        System.out.println("\nTampered Message: " + tamperedMessage);
-
-        boolean isTamperedVerified = verifySignature(tamperedMessage.getBytes(), digitalSignature, publicKey);
-        System.out.println("Tampered Message Verified: " + isTamperedVerified);
+            boolean verified = verifySignature(originalMessage.getBytes(), signatureBytes, publicKey);
+            System.out.println("Signature Verified: " + verified);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+    private static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
         keyPairGenerator.initialize(KEY_SIZE);
         return keyPairGenerator.generateKeyPair();
     }
 
-    public static byte[] signMessage(byte[] data, PrivateKey privateKey) throws Exception {
+    private static byte[] signMessage(byte[] data, PrivateKey privateKey) throws Exception {
         Signature signature = Signature.getInstance(ALGORITHM);
         signature.initSign(privateKey);
         signature.update(data);
         return signature.sign();
     }
 
-    public static boolean verifySignature(byte[] data, byte[] digitalSignature, PublicKey publicKey) throws Exception {
+    private static boolean verifySignature(byte[] data, byte[] digitalSignature, PublicKey publicKey) throws Exception {
         Signature signature = Signature.getInstance(ALGORITHM);
         signature.initVerify(publicKey);
         signature.update(data);
